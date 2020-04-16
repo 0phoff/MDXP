243b2fd43758413a15fc36521d8d2d768b8f240e
0phoff <0phoff@users.noreply.github.com>
2020-04-16 14:55:13 +0200

    Created quick appear component to test stepping

---
+++ packages/demo/src/appear.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {useStep, useDeck} from '@mdx-presenter/core';
+
+
+const Appear = ({children, ...props}) => {
+  const length = React.Children.count(children);
+  const step = useStep(length);
+
+  return (
+    <React.Fragment>
+      {
+        React.Children.map(children, (child, i) => {
+          return React.cloneElement(child, {style: {visibility: i <= step ? 'visible' : 'hidden'}});
+        })
+      }
+    </React.Fragment>
+  );
+};
+export default Appear;

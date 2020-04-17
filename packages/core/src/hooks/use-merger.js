23d9f4df2e344a3c52eb1610d7a5d918552ee7fe
0phoff <0phoff@users.noreply.github.com>
2020-04-17 17:20:47 +0200

    Added UseHOReducer
    Started slow process of documentation

--- packages/core/src/hooks/use-merger.js
+++ packages/core/src/hooks/use-merger.js
@@ -1,6 +1,15 @@
 import {useReducer} from 'react';
 
 
+/**
+ * Merging Reducer
+ * It takes a new state object and merges it with the old state, overwriting any properties with the new state values.
+ *
+ * @param   {object} defaultState
+ *          Default state the reducer starts with
+ * @return  {newState => void}
+ *          Reducer function which computes the new state by merging the old and new state objects
+ */
 const useMerger = (defaultState) => {
   return useReducer(
     (state, next) => {

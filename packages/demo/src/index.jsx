97d25708aa8854f7cb8ec25cbe99d234be91df1f
0phoff <0phoff@users.noreply.github.com>
2020-04-17 18:58:37 +0200

    Updated demo to proof that sibling useStep calls work, no matter the order

--- packages/demo/src/index.jsx
+++ packages/demo/src/index.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Deck from '@MDXP/core';
+
 import MDXPresentation from './presentation.mdx';
-
-
 import DummyLayout from './dummyLayout.jsx';
 
 

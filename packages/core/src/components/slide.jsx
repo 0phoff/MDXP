a3ef832ff0337f65d590cd49aacb324931c8de9d
0phoff <0phoff@users.noreply.github.com>
2020-04-17 09:40:41 +0200

    Removed useIndex hook
    It was only used in the slide component, so moved the function there

--- packages/core/src/components/slide.jsx
+++ packages/core/src/components/slide.jsx
@@ -4,17 +4,28 @@ import React from 'react';
 import useRoot from '../hooks/use-root.js';
 import useMerger from '../hooks/use-merger.js';
 import useKeyboard from '../hooks/use-keyboard.js';
-import useIndex from '../hooks/use-index.js';
 
 
 export const DeckContext = React.createContext(null);
 DeckContext.displayName = 'MDXP/DeckContext';
 
 
+const getIndex = (slide, slideLength) => {
+  if (slide >= slideLength) {
+    slide = slideLength - 1;
+  }
+  else if (slide < 0) {
+    slide = Math.max(0, slideLength + slide);
+  }
+
+  return slide;
+};
+
+
 const Slide = ({children, slide, reference, step=0, preview=false, sx={}}) => {
   // Data
   const rootContext = useRoot();
-  const slideIndex = useIndex(slide);
+  const slideIndex = getIndex(parseInt(slide), rootContext.slideLength);
   const [state, setState] = useMerger({
     mode: rootContext.mode,
     slideLength: rootContext.slideLength,

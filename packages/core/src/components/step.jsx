5092b56e2817a9f8a8d3cdb7b8438c8fba5d5555
0phoff <0phoff@users.noreply.github.com>
2020-04-18 23:06:59 +0200

    Step component now steps through rows/columns of tables
    Also updated demo to show the styles property

--- packages/core/src/components/step.jsx
+++ packages/core/src/components/step.jsx
@@ -2,10 +2,122 @@
 import {jsx} from 'theme-ui';
 import React, {useContext, useEffect, useReducer} from 'react';
 import useStep from '../hooks/use-step.js';
+import getComponentType from '../util/component-type.js';
 
 
 export const StepContext = React.createContext([{start: 0, length: 0}]);
 StepContext.displayName = 'MDXP/StepContext';
+
+
+const renderRegular = (children, createStyledElement) => (
+  <React.Fragment>
+    {React.Children.map(children, (child, i) => createStyledElement(child, i, {key: i}))}
+  </React.Fragment>
+);
+
+
+const renderSingle = (child, createStyledElement) => {
+  const children = child.props.children;
+
+  return (React.cloneElement(child, null,
+    React.Children.map(children, (child, i) => createStyledElement(child, i, {key: i}))
+  ));
+};
+
+
+const renderTable = (table, createStyledElement, {useColumns}) => {
+  const children = table.props.children;
+
+  if (useColumns) {
+    return React.cloneElement(table, null,
+      React.Children.map(children, (child) => {
+        if (getComponentType(child) === 'tr') {
+          let colNum = 0;
+          return React.cloneElement(child, null, 
+            React.Children.map(child.props.children, (child) => (
+              createStyledElement(child, colNum++)
+            ))
+          );
+        }
+        else {
+          return React.cloneElement(child, null, 
+            React.Children.map(child.props.children, (child) => {
+              let colNum = 0;
+              return React.cloneElement(child, null, 
+                React.Children.map(child.props.children, (child) => (
+                  createStyledElement(child, colNum++)
+                ))
+              );
+            })
+          );
+        }
+      })
+    );
+  }
+  else {
+    let rowNum = 0;
+    return React.cloneElement(table, null, 
+      React.Children.map(children, (child) => {
+        if (getComponentType(child) === 'tr') {
+          return createStyledElement(child, rowNum++);
+        }
+        else {
+          return React.cloneElement(child, null,
+            React.Children.map(child.props.children, (child) => (
+              createStyledElement(child, rowNum++)
+            ))
+          );
+        }
+      })
+    );
+  }
+};
+
+
+const getStepableChildren = (children, {useColumns}) => {
+  const length = React.Children.count(children);
+  if (length > 1 || !children.hasOwnProperty('props')) {
+    return [length, renderRegular];
+  }
+  else if (getComponentType(children) === 'table') {
+    if (useColumns) {
+      let colLength = 0;
+      React.Children.map(children.props.children, (child) => {
+        if (getComponentType(child) === 'tr') {
+          colLength = Math.max(colLength, React.Children.count(child.props.children));
+        }
+        else {
+          React.Children.map(child.props.children, (child) => {
+            colLength = Math.max(colLength, React.Children.count(child.props.children));
+          });
+        }
+      });
+      return [colLength, renderTable];
+    }
+    else {
+      let rowLength = 0;
+      React.Children.map(children.props.children, (child) => {
+        if (getComponentType(child) === 'tr') {
+          rowLength++;
+        }
+        else {
+          React.Children.map(child.props.children, (child) => {
+            if (getComponentType(child) === 'tr') {
+              rowLength++;
+            }
+          });
+        }
+      });
+      return [rowLength, renderTable];
+    }
+  }
+  else {
+    return [
+      React.Children.count(children.props.children),
+      renderSingle
+    ];
+  }
+};
 
 
 /**
@@ -13,33 +125,43 @@ StepContext.displayName = 'MDXP/StepContext';
  * This component generates a step for each of its direct children
  * and can style them depending on their respective order and the current stepIndex.
  * The most common use case is to make items appear or disappear in order.
- * If this component only has one child, it will implement the stepping behaviour on it's children.
  *
- * TODO : add params
+ * If this component only has one child, it will implement the stepping behaviour on it's children.
+ * If the child component happens to be a table, it will step through its rows or columns,
+ * depending on the `useColumns` parameter.
+ *
+ * @param   {Integer} offset
+ *          Offset to the stepping algorithm; Usually the number of elements you want to show at start (default: 1)
+ * @param   {Boolean} useSx
+ *          Whether to apply the styles as a `style` or `sx` property (default: false)
+ * @param   {Boolean} useColumns
+ *          [Tables only] Whether to step through rows or columns of a table (default: false)
+ * @param   {object} styles
+ *          styles to apply to the child elements
+ * @param   {object} styles.before
+ *          styles to apply to the child elements before the step reaches them (default: {visibility: 'hidden'})
+ * @param   {object} styles.after
+ *          styles to apply to the child elements after the step has reached them (default: {visibility: 'visible'})
+ * @param   {object} styles.current
+ *          styles to apply to the child elements when the step reaches them (default: same as `styles.after`)
+ * @param   {object} styles.base
+ *          styles to apply to all child elements
+ *
  * TODO : add examples
  */
 const Step = ({
   children,
   offset=1,
-  styles={before: {'visibility': 'hidden'}, after: {'visibility': 'visible'}},
   useSx=false,
+  useColumns=false,
+  styles={before: {visibility: 'hidden'}, after: {visibility: 'visible'}},
 }) => {
   // Figure out children
-  let rootChild;
-  length = React.Children.count(children);
-  if (length == 1 && children.props && children.props.hasOwnProperty('children')) {
-    rootChild = React.Children.only(children);
-    children = rootChild.props.children;
-    length = React.Children.count(children);
-  }
-
-  // Setup steps
+  const [length, render] = getStepableChildren(children, {useColumns});
   offset = parseInt(offset);
   const step = useStep(length + offset) - offset;
-
-  // Functions
   const createStyledElement = (child, i, props) => {
-    let style = child.props.style;
+    let style = useSx ? child.props.sx : child.props.style;
     if (i > step) {
       style = {
         ...style,
@@ -73,17 +195,6 @@ const Step = ({
     return React.cloneElement(child, childProps);
   };
 
-  if (rootChild) {
-    return (React.cloneElement(rootChild, null,
-      React.Children.map(children, (child, i) => createStyledElement(child, i, {key: i}))
-    ));
-  }
-  else {
-    return (
-      <React.Fragment>
-        {React.Children.map(children, (child, i) => createStyledElement(child, i, {key: i}))}
-      </React.Fragment>
-    );
-  }
+  return render(children, createStyledElement, {useColumns});
 };
 export default Step;

7f08631ef4cce0aa4424c56c1c34635694d732f2
0phoff <0phoff@users.noreply.github.com>
2020-04-30 15:13:26 +0200

    Updated demo to use <HeaderLayout> per default

--- examples/demo/src/index.jsx
+++ examples/demo/src/index.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+/** @jsx jsx */
+import {jsx} from 'theme-ui';
 import ReactDOM from 'react-dom';
-import Deck from '@mdxp/core';
-
+import Deck, {useDeck} from '@mdxp/core';
+import {HeaderLayout} from '@mdxp/components';
 import MDXPresentation from './presentation.mdx';
 
+const Footer = () => {
+  const {slideIndex} = useDeck();
+
+  return (
+    <div
+      sx={{
+        width: '100%',
+        px: '5%',
+        py: '10px',
+        bg: 'secondary',
+        color: 'background',
+        textAlign: 'right',
+        fontSize: '1.5em'
+      }}
+    >
+      {slideIndex + 1}
+    </div>
+  );
+};
+
 ReactDOM.render(
-  <Deck>
+  <Deck Layout={HeaderLayout} layoutOptions={{sxHeader: {width: '100%', bg: 'text', color: 'background'}, Footer}}>
     <MDXPresentation />
   </Deck>,
   document.getElementById('root')

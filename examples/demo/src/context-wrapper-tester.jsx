6a9c021670b02a0de1d1dca60abed3f827125edf
0phoff <0phoff@users.noreply.github.com>
2020-04-30 22:31:05 +0200

    Updated demo

--- examples/demo/src/context-wrapper-tester.jsx
+++
@@ -1,9 +0,0 @@
-import React, {useContext} from 'react';
-import {ContextWrapperContext} from './context-wrapper.jsx';
-
-const ContextWrapperTester = ({idx}) => {
-  const value = useContext(ContextWrapperContext);
-  return (<p>{value[idx]}</p>);
-};
-
-export default ContextWrapperTester;

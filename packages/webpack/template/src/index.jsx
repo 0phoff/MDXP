fa68feb48e1d35b5b7e235b16f247b03387f6944
0phoff <0phoff@users.noreply.github.com>
2020-05-04 17:37:01 +0200

    V0.0.1 documentation

--- packages/webpack/template/src/index.jsx
+++ packages/webpack/template/src/index.jsx
@@ -1,35 +1,12 @@
 /** @jsx jsx */
 import {jsx} from 'theme-ui';
 import ReactDOM from 'react-dom';
-import Deck, {useDeck} from '@mdxp/core';
-import {HeaderLayout} from '@mdxp/components';
+import Deck from '@mdxp/core';
+import components from '@mdxp/components';
 import MDXPresentation from './presentation.mdx';
 
-const Footer = () => {
-  const {slideIndex} = useDeck();
-
-  return (
-    <div
-      sx={{
-        width: '100%',
-        px: '5%',
-        py: '10px',
-        bg: 'secondary',
-        color: 'background',
-        textAlign: 'right',
-        fontSize: '1.5em'
-      }}
-    >
-      {slideIndex + 1}
-    </div>
-  );
-};
-
 ReactDOM.render(
-  <Deck
-    Layout={HeaderLayout}
-    layoutOptions={{sxHeader: {width: '100%', bg: 'text', color: 'background'}, Footer}}
-  >
+  <Deck components={components}>
     <MDXPresentation />
   </Deck>,
   document.getElementById('root')

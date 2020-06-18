8122f9a5657f83e7c0fd8271bcf642d4e53f5631
0phoff <0phoff@users.noreply.github.com>
2020-06-18 19:20:04 +0200

    FIX demo with regard to latest changes in MDXP packages
    Also removed accidentally added .demo-old

--- examples/demo/src/index.jsx
+++ examples/demo/src/index.jsx
@@ -2,7 +2,7 @@
 import {jsx} from 'theme-ui';
 import ReactDOM from 'react-dom';
 import Deck, {Zoom} from '@mdxp/core';
-import components from '@mdxp/components';
+import * as components from '@mdxp/components';
 
 import theme from './theme/theme.js';
 import themeComponents  from './theme/theme-components.js';
@@ -15,6 +15,7 @@ ReactDOM.render(
     maxWidth={1000}
     width={1000}
     aspectRatio={16/9}
+    sx={{maxWidth: '100vw', maxHeight: '100vh'}}
   >
     <Deck
       components={{...components, ...themeComponents}}
